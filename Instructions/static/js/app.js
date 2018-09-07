de901c60a05b16303017d3dfab168163b2d91995
KCKaiser <kc7mt@virginia.edu>
2018-09-06 23:01:32 -0400

    Weird wide web

---
+++ Instructions/static/js/app.js
@@ -0,0 +1,45 @@
+// DATASET 
+var tableData = data;
+
+// SELECT TABLE (tbody) - 
+var tbody = d3.select("tbody");
+
+// INSERT DATA TO tbody 
+function insertData(data){
+    var row = tbody.append("tr");
+    Object.entries(data).forEach(([key, value]) => {
+        var cell = tbody.append("td"); 
+        cell.text(value.toString());
+    })
+};
+
+tableData.forEach(insertData);
+
+// CREATING FUNCTION TO CLEAR tobody and REPLACE WITH FILTERED DATA
+
+function clearAndReplace(filteredData){ 
+    d3.select("tbody").html("");
+    filteredData.forEach(insertData);
+};
+
+// Filtering by date
+var button = d3.select("#filter-btn");
+var inputBar = d3.select(".form-control");
+
+button.on("click", function (){
+    d3.event.preventDefault();
+    var inputValue = inputBar.property("value");
+    console.log(inputValue);
+    var filteredData = tableData.filter(data => data.datetime == inputValue);
+
+    clearAndReplace(filteredData);    
+})
+
+function show_more ( element_to_show ) {
+    var element_to_show = document.getElementById("more");
+    element_to_show.style.display = "inline";
+    }
+
+console.log(inputBar);
+
+
